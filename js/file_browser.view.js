49547933f3d19b93e21ace3f295e7ac17d6acade
samuel.mortenson <samuel.mortenson@2582268.no-reply.drupal.org>
2016-05-13 09:37:44 -0700

    Issue #2718897 by Bcwald, samuel.mortenson: Take advantage of imagesLoaded to improve UX of loading images

--- js/file_browser.view.js
+++ js/file_browser.view.js
@@ -11,15 +11,22 @@
 
   Drupal.behaviors.FileBrowserView = {
     attach: function (context) {
-      $('.view-content').prepend('<div class="grid-sizer"></div><div class="gutter-sizer"></div>').once();
-      $('.view-content').imagesLoaded(function () {
-        $('.view-content').masonry({
+      var $view = $('.view-content');
+      $view.prepend('<div class="grid-sizer"></div><div class="gutter-sizer"></div>').once();
+
+      // Indicate that images are loading.
+      $view.append('<div class="ajax-progress ajax-progress-fullscreen">&nbsp;</div>');
+      $view.imagesLoaded(function () {
+        $view.masonry({
           columnWidth: '.grid-sizer',
           gutter: '.gutter-sizer',
           itemSelector: '.grid-item',
           percentPosition: true,
           isFitWidth:true
         });
+        // Add a class to reveal the loaded images, which avoids FOUC.
+        $('.grid-item').addClass('item-style');
+        $view.find('.ajax-progress').remove();
       });
 
       $('.grid-item').once('bind-click-event').click(function () {
